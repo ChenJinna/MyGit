f3dde2d39af06f3218b35076e2e31942820dfe54
jinna.chen <jinna.chen@g-emall.com>
2017-04-12 10:56:00 +0800

    add feature-1 on feature-1 branch

--- app/src/main/java/com/kina/mygit/MainActivity.java
+++ app/src/main/java/com/kina/mygit/MainActivity.java
@@ -1,13 +1,26 @@
 package com.kina.mygit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_feature_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_feature_1 = (Button) findViewById(R.id.btn_feature_1);
+    }
+
+    public void feature_1(View view) {
+        if (btn_feature_1.getText().toString().equals("功能1")) {
+            btn_feature_1.setText("修改");
+        } else {
+            btn_feature_1.setText("原本");
+        }
     }
 }
